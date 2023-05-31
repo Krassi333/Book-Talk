9d6635c39f379a86c538aa6e8a8dacc3505f3036
Krasimira Valcheva <krasimira.valcheva88@gmail.com>
2023-05-31 13:28:47 +0300

    Create register controller

---
+++ controllers/authController.js
@@ -0,0 +1,12 @@
+const { body, validationResult } = require('express-validator');
+const router = require('express').Router();
+const { register, login } = require('../services/userServices');
+const { parseError } = require('../util/parser');
+
+router.get('/register', (req, res) => {
+    res.render('register', {
+        title: "Register Page"
+    })
+});
+
+module.exports = router;
