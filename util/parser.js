b7bd107ea8061cce37c68e19a2501ab19159e566
Krasimira Valcheva <krasimira.valcheva88@gmail.com>
2023-05-31 13:28:13 +0300

    Create error parser

---
+++ util/parser.js
@@ -0,0 +1,14 @@
+function errorParser(error) {
+    if (error.name == 'ValidationError') {
+        return Object.values(error.errors).map(v => v.message); //хваща грешките от mongoose(от моделите)
+    } else if (Array.isArray(error)) {  //хваща грешките от express-validator
+        return error.map(x => x.msg);
+    } else {
+        return error.message.split('\n');
+    }
+
+}
+
+module.exports = {
+    errorParser
+}
