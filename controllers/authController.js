2732ffbab8cde38575900acf7ff57417fc80346c
Krasimira Valcheva <krasimira.valcheva88@gmail.com>
2023-05-31 13:29:30 +0300

    Create register page functionality

--- controllers/authController.js
+++ controllers/authController.js
@@ -9,4 +9,45 @@ router.get('/register', (req, res) => {
     })
 });
 
+router.post('/register',
+    body('username')
+        .isLength({ min: 4 }).withMessage('Username must be at least 4 characters long!'),
+    body('password')
+        .isLength({ min: 3 }).withMessage('Password must be at least 3 characters long!'),
+    async (req, res) => {
+        try {
+            const { errors } = validationResult(req);
+
+            if (errors.length > 0) {
+                throw errors;
+            }
+
+            if (req.body.username == '' || req.body.password == '' || req.body.email == '') {
+                throw new Error('All fields are required!');
+            }
+
+            if (req.body.password != req.body.repass) {
+                throw new Error('Passwords don\'t match!');
+            }
+
+            const token = await register(req.body.username, req.body.email, req.body.password);
+
+            res.cookie('token', token);
+
+            res.redirect('/');
+        } catch (err) {
+
+            const errors = parseError(err);
+
+            res.render('register', {
+                title: "Register Page",
+                errors,
+                body: {
+                    username: req.body.username,
+                    email: req.body.email
+                }
+            })
+        }
+    });
+
 module.exports = router;
