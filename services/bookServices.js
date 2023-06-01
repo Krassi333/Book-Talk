4f4d961c6e26a6bf840c8bd2c27cc1b96291d3d0
Krasimira Valcheva <krasimira.valcheva88@gmail.com>
2023-06-01 23:16:47 +0300

    Create add book service

--- services/bookServices.js
+++ services/bookServices.js
@@ -3,6 +3,10 @@ const Book = require('../models/Book');
 async function getAllBooks() {
     return Book.find({}).collation({ locale: 'en', strength: 2 }).lean();
 }
+async function createBook(data) {
+    console.log('data in service '+ data);
+    return Book.create(data);
+};
 
 module.exports = {
     getAllBooks,
