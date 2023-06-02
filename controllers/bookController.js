db7ced16c2e1cbdf3a5d1dc6f4b2e8e63960bf9d
Krasimira Valcheva <krasimira.valcheva88@gmail.com>
2023-06-02 23:56:26 +0300

    Create add book functionality

--- controllers/bookController.js
+++ controllers/bookController.js
@@ -18,4 +18,33 @@ router.get('/create', async (req, res) => {
     })
 });
 
+router.post('/create', async (req, res) => {
+
+    const data = {
+        title: req.body.title,
+        author: req.body.author,
+        imageUrl: req.body.imageUrl,
+        bookReview: req.body.bookReview,
+        genre: req.body.genre,
+        stars: Number(req.body.stars),
+        owner: req.user._id
+    }
+   // console.log(data);
+
+    try {
+        await createBook(data);
+        res.redirect('/book/catalog');
+    } catch (err) {
+        const errors = errorParser(err);
+
+        res.render('create', {
+            title: 'Create Page',
+            errors,
+            data
+        })
+    }
+})
+
+
+
 module.exports = router;
